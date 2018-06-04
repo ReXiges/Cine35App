cc7d72ed9b089a71b9ffc2bfe708ff4f176580af
kevinjg03 <mkjg16@gmail.com>
2018-06-04 03:07:43 -0600

    Add files via upload

--- app/src/main/java/cine35app/esteb/cine35app/InsertMovieActivity.java
+++ app/src/main/java/cine35app/esteb/cine35app/InsertMovieActivity.java
@@ -1,16 +1,18 @@
 package cine35app.esteb.cine35app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.List;
+import java.io.IOException;
+
+import okhttp3.Response;
 
 public class InsertMovieActivity extends AppCompatActivity {
     private EditText movieName;
@@ -22,10 +24,6 @@ public class InsertMovieActivity extends AppCompatActivity {
     private Button saveMovie;
     private Button cancel;
     private TextView info;
-    private List<Pelicula> movies;
-    private RecyclerView recyclerView;
-    private GridLayoutManager gridLayout;
-    private MoviesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,11 +60,29 @@ public class InsertMovieActivity extends AppCompatActivity {
         {
             info.setText("Information is not valid, please check!");
         }
-        else
-        {
-            Intent pIntent = new Intent(InsertMovieActivity.this, AdmiActivity.class);
-            startActivity(pIntent);
+         else {
+        Queryphp query = new Queryphp("searchMovie.php?nombre='" + movieName + "'");
+        try {
+            Response response = query.returnRequest();
+            if (!response.body().string().equals("[]")) {
+                info.setText("Movie already in System");
+            } else {
+                query.setQuery("insertMovie.php?&nombre='" + movieName + "'&anio='" + year + "'$keywords='" + "'a,a,a''" + "'&actores='" + actores + "'&directores='" + director + "'&imagen='" + "aass" + "'&genero='" + gender + "'");
+                response = query.returnRequest();
+                Context context = getApplicationContext();
+                CharSequence text = "Movie registered";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                Intent pIntent = new Intent(InsertMovieActivity.this, AdmiActivity.class);
+                startActivity(pIntent);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
     }
 
     private void cancelFunction()
